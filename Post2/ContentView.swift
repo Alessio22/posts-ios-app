7872dea9c66cc5b8f781fbeb4f6bec3a09820458
Alessio Canestrelli <a.canestrelli@gmail.com>
2020-11-13 08:52:42 +0100

    Initial Commit

---
+++ Post2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Post2
+//
+//  Created by Alessio Canestrelli on 13/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Post2/Post2App.swift
@@ -0,0 +1,17 @@
+//
+//  Post2App.swift
+//  Post2
+//
+//  Created by Alessio Canestrelli on 13/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Post2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
